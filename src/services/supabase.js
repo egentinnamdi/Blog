6aff40952f229d599366ad6b1e8a0869f83f1484
Nnamdi James <egentinnamdi10@gmail.com>
2024-11-02 16:07:30 +0100

    supabase

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -5,7 +5,7 @@ const url = "https://ljroxogsifnbeofppyii.supabase.co";
 const key =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxqcm94b2dzaWZuYmVvZnBweWlpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjcxMzIxMjMsImV4cCI6MjA0MjcwODEyM30.a2E8aOE3IiNbs_ts9-Zbl6_qkGAoGUiH_W4dNFq0u-8";
 
-const supabase = await createClient(url, key);
+const supabase = createClient(url, key);
 
 async function getPosts() {
   const { data, error } = await supabase.from("blog-posts").select("*");
